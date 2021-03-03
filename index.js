4ac3e6a6f9de8b649f432f7fbcf7e1a13e5c13c8
hkgnp <ben@pngs.cc>
2021-03-03 10:29:26 +0800

    Implemented posting to new collection

--- index.js
+++ index.js
@@ -1,51 +1,51 @@
-// async function upload(objectId) {
-//     const file = document.getElementById('thefile').files[0];
-
-//     // Make sure a file is selected
-//     if (!file) return;
-//     // Fetch the signed url
-//     const key = objectId + "_" + file.name;
-//     const response = await axios.get(`https://quiet-gorge-29042.herokuapp.com/uploader/sign?key=${key}&type=${file.type}`);
-//     const url = response.data.url;
-
-//     try {
-//         // Attempt the upload
-//         const options = { headers: { 'Content-Type': file.type } };
-//         await axios.put(url, file, options);
-//     } catch (e) {
-//         alert(`Upload failed: ${e}`);
-//     }
-// }
-
 let sendForm = async (req, res) => {
-    let formResponse = await axios({
-        method: 'post',
-        url: 'https://7000-yellow-lion-m3odfn59.ws-us03.gitpod.io/posts',
-        data: {
-            "title": document.querySelector("#title").value,
-            "description": document.querySelector("#description").value,
-            "category": document.querySelector("#category").value,
-            "location": document.querySelector("#location").value
-        }
-    });
+  /// Send to collection 'POST DETAILS'
+  let postDetails = await axios({
+    method: 'post',
+    url: 'https://7000-yellow-lion-m3odfn59.ws-us03.gitpod.io/posts',
+    data: {
+      title: document.querySelector('#title').value,
+      description: document.querySelector('#description').value,
+      category: document.querySelector('#category').value,
+      location: document.querySelector('#location').value,
+    },
+  });
 
-    let objectId = await formResponse.data
-    const file = document.getElementById('thefile').files[0];
-    // Make sure a file is selected
-    if (!file) return;
-    // Fetch the signed url
-    const key = objectId + "_" + file.name;
-    const response = await axios.get(`https://7000-yellow-lion-m3odfn59.ws-us03.gitpod.io/uploader/sign?key=${key}&type=${file.type}`);
-    const url = response.data.url;
+  /// Send to S3
+  let postObjectId = await postDetails.data;
+  const file = document.getElementById('thefile').files[0];
+  // Make sure a file is selected
+  if (!file) return;
+  // Fetch the signed url
+  const key = postObjectId + '_' + file.name;
+  const response = await axios.get(
+    `https://7000-yellow-lion-m3odfn59.ws-us03.gitpod.io/uploader/sign?key=${key}&type=${file.type}`
+  );
+  const url = response.data.url;
 
-    try {
-        // Attempt the upload
-        const options = { headers: { 'Content-Type': file.type } };
-        await axios.put(url, file, options);
-    } catch (e) {
-        alert(`Upload failed: ${e}`);
-    }
-    alert("Post successful");
+  try {
+    // Attempt the upload
+    const options = { headers: { 'Content-Type': file.type } };
+    await axios.put(url, file, options);
+  } catch (e) {
+    alert(`Upload failed: ${e}`);
+  }
 
-}
-
+  // Send S3 URL to collection 'MEDIA
+  // - Include ObjectID of post-details
+  // data: {media: "https://msw-keeposted-images.s3-ap-southeast-1.amazonaws.com/" + objectId + '_' + file.name}
+  let mediaDetails = await axios({
+    method: 'post',
+    url: 'https://7000-yellow-lion-m3odfn59.ws-us03.gitpod.io/media',
+    data: {
+      postId: postObjectId,
+      mediaUrl:
+        'https://msw-keeposted-images.s3-ap-southeast-1.amazonaws.com/' +
+        objectId +
+        '_' +
+        file.name,
+    },
+  });
+  console.log(mediaDetails);
+  alert('Post successful');
+};
